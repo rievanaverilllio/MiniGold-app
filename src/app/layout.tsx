dc8d4e1af22e576033db512da9c007a4f34b8f87
rievan <averillio.rievan@gmail.com>
2025-09-07 18:06:54 +0700

    add dashboard admin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Playfair_Display, Inter } from "next/font/google";
 import "./globals.css";
-import LenisProvider from "@/hook/LenisProvider";
+import LenisProvider from "@/hooks/LenisProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
