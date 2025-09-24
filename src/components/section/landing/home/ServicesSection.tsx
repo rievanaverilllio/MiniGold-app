6a1810be8511afe19da7a932bd2df3da99eb6c17
rievan <averillio.rievan@gmail.com>
2025-09-24 20:51:12 +0700

    update grid service

--- src/components/section/landing/home/ServicesSection.tsx
+++ src/components/section/landing/home/ServicesSection.tsx
@@ -47,7 +47,7 @@ const ServicesSection = () => {
   return (
     <section className="py-5 bg-gray-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+  <div className="grid grid-cols-2 [@media(max-width:360px)]:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {services.map((service) => (
             <div key={service.id} className="text-center">
               {/* Icon */}
