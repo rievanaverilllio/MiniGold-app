4c76b4056b6b0290d290f59303839df439d8bef6
rievan <averillio.rievan@gmail.com>
2025-09-24 20:36:23 +0700

    update grid

--- src/components/section/landing/home/BestSellersSection.tsx
+++ src/components/section/landing/home/BestSellersSection.tsx
@@ -27,7 +27,7 @@ const BestSellersSection = () => {
         </div>
 
         {/* Products Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3">
+  <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-3">
           {products.map((product) => (
             <Link
               key={product.id}
