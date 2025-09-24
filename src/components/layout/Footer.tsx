90b2996b452988a94dc0e1a76f7cef5fdb6d3a4d
rievan <averillio.rievan@gmail.com>
2025-09-24 20:54:13 +0700

    update grid footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -16,7 +16,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-50 py-16">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+  <div className="grid grid-cols-2 [@media(max-width:360px)]:grid-cols-1 md:grid-cols-4 lg:grid-cols-4 gap-8">
           
           {/* Pages Column */}
           <div>
@@ -90,7 +90,7 @@ const Footer = () => {
           </div>
 
           {/* Newsletter Subscription Column */}
-          <div>
+          <div className="col-span-2 md:col-span-2 lg:col-span-1">
             <h3 className="text-sm font-semibold text-gray-900 uppercase tracking-wider mb-4">
               10% OFF YOUR FIRST PURCHASE
             </h3>
